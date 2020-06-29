6ec4dda72d15d4e8482692d82e0396271003da70
Julian Lagerberg <jl@jlagerberg.se>
2020-06-29 19:52:51 +0200

    Fix build error

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,9 +5,15 @@ export default {
    */
   mode: 'universal',
   /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: 'static',
+  /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
    */
+
   head: {
     title: 'SurveyGenerator',
     meta: [
