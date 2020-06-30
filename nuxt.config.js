3d99ecb0735b6d8d790fb65d57752bc2f12599d8
Julian Lagerberg <jl@jlagerberg.se>
2020-06-30 16:00:19 +0200

    Test 3

--- nuxt.config.js
+++ nuxt.config.js
@@ -88,6 +88,6 @@ export default {
     },
   },
   generate: {
-    fallback: true, // Netlify reads a 404.html, Nuxt will load as an SPA
+    fallback: '404.html', // Netlify reads a 404.html, Nuxt will load as an SPA
   },
 }
