a53b6a55592588a40cda661cb40fe17917bd7426
Julian Lagerberg <jl@jlagerberg.se>
2020-06-30 12:58:01 +0200

    Test 1

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'server',
+  target: 'static',
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
@@ -88,6 +88,6 @@ export default {
     },
   },
   generate: {
-    fallback: '404.html', // Netlify reads a 404.html, Nuxt will load as an SPA
+    fallback: true, // Netlify reads a 404.html, Nuxt will load as an SPA
   },
 }
