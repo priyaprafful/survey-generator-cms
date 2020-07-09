9785e38ebb2b0490542ad65653fa197d1e2dd1f4
Julian Lagerberg <jl@jlagerberg.se>
2020-07-09 15:39:31 +0200

    Features text

--- components/Home/Features.vue
+++ components/Home/Features.vue
@@ -1,7 +1,52 @@
 <template>
   <section class="bg-blue-dark text-white">
-    <div class="flex justify-center max-w-screen-xl m-auto p-12">
-      <p>Features</p>
+    <div class="flex flex-col max-w-screen-xl m-auto p-24 px-12">
+      <h2 class="mb-12">Från ax till limpa</h2>
+      <div class="flex leading-relaxed">
+        <Block half left>
+          <Placeholder class="w-12 mb-4" />
+          <p>
+            Vi är experter på webbundersökningar och bjuder gärna på vår
+            kunskap. Vårt enkätverktyg SurveyGenerator är känt för att vara
+            användarvänligt. Men, om du har ont om tid eller har specifika krav
+            kan vi ge dig allt stöd som du behöver beträffande både teknik och
+            kompetens.
+          </p>
+        </Block>
+        <Block half right>
+          <Placeholder class="w-12 mb-4" />
+          <p>
+            Stärk dina kunskaper kring webbundersökningar med våra utbildningar.
+            Du kanske behöver en grundlig genomgång så att du kan använda det på
+            absolut bästa sätt? Om inte tiden räcker till kan vi göra hela eller
+            delar av jobbet med själva genomförandet av en undersökning, och har
+            du speciella behov kan vi ta fram speciella lösningar.
+          </p>
+        </Block>
+      </div>
+      <div class="flex justify-between mt-8">
+        <p>
+          <b>Skräddarsytt</b>
+          Tycker du att de vanliga versionerna har nästan allt – men ändå inte
+          riktigt? Då kan vi skräddarsy verktyget till att passa just dig och
+          dina behov. Vi utformar helt enkelt dina enkäter så att de ser ut och
+          fungerar precis som du vill.
+        </p>
+        <p>
+          <b>Konsulthjälp</b>
+          Har du inte tid eller möjlighet att genomföra hela eller delar av
+          undersökningen själv kan vi hjälpa dig. Vi kan också ge dig råd kring
+          alternativa användningsområden av enkätverktyget för att kunna nyttja
+          det maximalt.
+        </p>
+        <p>
+          <b>Mjukvaruutveckling</b>
+          Ibland krävs det lite mer. Du kanske behöver en unik lösning för just
+          din verksamhet som helt enkelt inte finns ännu. Då kan vi hjälpa dig
+          med att utveckla helt ny mjukvara för just dina behov. Vi arbetar
+          främst i Microsoftmiljö.
+        </p>
+      </div>
     </div>
   </section>
 </template>
