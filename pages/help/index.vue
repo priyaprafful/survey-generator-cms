17428e11a0a7dc405f68d199404a0152ffefde7e
Julian Lagerberg <jl@jlagerberg.se>
2020-11-17 16:10:51 +0100

    Help page initial commit

---
+++ pages/help/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <Wrapper>
+    <p>Hjälpen</p>
+  </Wrapper>
+</template>
+
+<script>
+export default {
+  nuxtI18n: {
+    paths: {
+      sv: '/hjalp',
+      en: '/help',
+    },
+  },
+  /* async asyncData({ $prismic, error, app }) {
+    const currentLocale = app.i18n.locales.filter(
+      (lang) => lang.code === app.i18n.locale
+    )[0];
+
+    try {
+      // Query to get blog home content
+      const quoteContent = (
+        await $prismic.api.getSingle('get_quote', {
+          lang: currentLocale.iso.toLowerCase(),
+        })
+      ).data;
+
+      // Returns data to be used in template
+      return {
+        quoteContent,
+      };
+    } catch (e) {
+      // Returns error page
+      error({ statusCode: 404, message: 'Page not found' });
+    }
+  }, */
+  head() {
+    return {
+      title: this.$t('menu.help') + ' — SurveyGenerator',
+    };
+  },
+};
+</script>
