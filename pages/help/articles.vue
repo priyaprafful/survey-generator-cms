7f59f58a389b3cf3d8573a1a3e9d6e611a7a75b0
Julian Lagerberg <jl@jlagerberg.se>
2020-11-20 14:27:39 +0100

    Help faq data

--- pages/help/articles.vue
+++ pages/help/articles.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>Artiklar !!!</div>
+  <div>
+    <p>Artiklar !!!</p>
+    <HelpCTA />
+  </div>
 </template>
 
 <script>
+import HelpCTA from '@/components/Help/HelpCTA.vue';
+
 export default {
   nuxtI18n: {
     paths: {
       sv: '/hjalpen/artiklar',
       en: '/help/articles',
     },
+  },
+  components: {
+    HelpCTA,
   },
   head() {
     return {
