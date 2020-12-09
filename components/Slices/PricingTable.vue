006392ff7ddb68565e1b1ab913d0be855b7b8d39
Julian Lagerberg <jl@jlagerberg.se>
2020-12-09 11:14:10 +0100

    PricingTable first row

--- components/Slices/PricingTable.vue
+++ components/Slices/PricingTable.vue
@@ -14,64 +14,72 @@
         </tr>
       </thead>
 
-      <!-- <tbody
-        v-for="(feature, index) in data.features"
+      <tbody
+        v-for="(feature, index) in slice.items"
         :key="index"
         class="even:bg-bluegray-1 odd:bg-white"
       >
-        <tr v-if="feature.group_title.length > 0" class="bg-blue-9 text-white">
+        <tr v-if="feature.group_title[0]" class="bg-blue-9 text-white">
           <th class="p-4 text-left font-medium">
             <prismic-rich-text :field="feature.group_title" />
           </th>
-          <td v-for="(card, i) in data.cards.slice(0, 3)" :key="i">
-            <prismic-rich-text :field="card.plan" />
+          <td v-for="(plan, i) in slice.primary.plans" :key="i">
+            <p>{{ plan.text }}</p>
           </td>
         </tr>
 
         <tr class="hover:bg-lightblue-1 transition duration-150 ease-in-out">
           <th class="p-4 text-left font-normal">
-            <prismic-rich-text :field="feature.title" />
+            <prismic-rich-text :field="feature.row_title" />
           </th>
-          <td v-if="feature.small_text.length < 1" class="p-4">
+          <td class="p-4">
             <img
-              v-if="feature.small_check"
-              src="@/assets/svgs/checkcircle.svg"
+              v-if="feature.first"
+              src="@/assets/svg/checkcircle.svg"
               alt=""
               class="w-4 h-4 m-auto"
             />
-            <svg-icon v-else icon="minus" lg class="text-bluegray-6" />
+            <img
+              v-if="!feature.first && !feature.first_text"
+              src="@/assets/svg/minus.svg"
+              alt=""
+              class="w-4 h-4 m-auto"
+            />
+            {{ feature.first_text }}
           </td>
-          <td v-if="feature.small_text.length > 0" class="p-4">
-            <prismic-rich-text :field="feature.small_text" />
-          </td>
+          <!-- <td v-if="feature.first_text.length > 0" class="p-4">
+            <prismic-rich-text :field="feature.first_text" />
+          </td> -->
 
-          <td v-if="feature.medium_text.length < 1" class="p-4">
+          <!-- <td v-if="feature.second_text.length < 1" class="p-4">
             <img
-              v-if="feature.medium_check"
-              src="@/assets/svgs/checkcircle.svg"
+              v-if="feature.second_check"
+              src="@/assets/svg/checkcircle.svg"
               alt=""
               class="w-4 h-4 m-auto"
             />
+            -
             <svg-icon v-else icon="minus" lg class="text-bluegray-6" />
           </td>
-          <td v-if="feature.medium_text.length > 0" class="p-4">
-            <prismic-rich-text :field="feature.medium_text" />
-          </td>
+          <td v-if="feature.second_text.length > 0" class="p-4">
+            <prismic-rich-text :field="feature.second_text" />
+          </td> -->
 
-          <td v-if="feature.large_text.length < 1" class="p-4">
+          <!-- <td v-if="feature.third_text.length < 1" class="p-4">
             <img
-              v-if="feature.large_check"
-              src="@/assets/svgs/checkcircle.svg"
+              v-if="feature.third_check"
+              src="@/assets/svg/checkcircle.svg"
               alt=""
               class="w-4 h-4 m-auto"
             />
+            -
             <svg-icon v-else icon="minus" lg class="text-bluegray-6" />
           </td>
-          <td v-if="feature.large_text.length > 0" class="p-4">
-            <prismic-rich-text :field="feature.large_text" />
-          </td>
+          <td v-if="feature.third_text.length > 0" class="p-4">
+            <prismic-rich-text :field="feature.third_text" />
+          </td> -->
         </tr>
-      </tbody> -->
+      </tbody>
     </table>
   </BaseSection>
 </template>
